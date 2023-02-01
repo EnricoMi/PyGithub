37fe2c1ca1ae0e2f4f998f486ec23b98e42e1251
Enrico Minack <github@enrico.minack.dev>
2023-02-01 11:01:49 +0100

    Update attributes if they exist

--- scripts/add_attribute.py
+++ scripts/add_attribute.py
@@ -8,6 +8,7 @@
 # Copyright 2016 Peter Buckley <dx-pbuckley@users.noreply.github.com>          #
 # Copyright 2018 sfdye <tsfdye@gmail.com>                                      #
 # Copyright 2018 bbi-yggy <yossarian@blackbirdinteractive.com>                 #
+# Copyright 2023 Enrico Minack <github@enrico.minack.dev>                      #
 #                                                                              #
 # This file is part of PyGithub.                                               #
 # http://pygithub.readthedocs.io/                                              #
@@ -82,6 +83,8 @@ newLines = []
 i = 0
 
 added = False
+existed = False
+updated = False
 
 isCompletable = True
 isProperty = False
@@ -97,27 +100,57 @@ while not added:
         attrName = line[8:-7]
         # Properties will be inserted after __repr__, but before any other function.
         if attrName != "__repr__" and (
-            attrName == "_identity" or attrName > attributeName or not isProperty
+            attrName == "_identity" or attrName >= attributeName or not isProperty
         ):
-            if not isProperty:
-                newLines.append("    @property")
-            newLines.append("    def " + attributeName + "(self):")
-            newLines.append('        """')
-            newLines.append("        :type: " + attributeDocType)
-            newLines.append('        """')
+            # collect lines existing for this property and those new lines we generate
+            # to compare them and detect an update
+            newPropLines = []
+            existingLines = []
+
+            if attrName == attributeName:
+                # consume all existing lines defining this property
+                if isProperty:
+                    existingLines.append("    @property")
+                while line.startswith("    def ") or line.startswith('        '):
+                    existingLines.append(line)
+                    line = lines[i].rstrip()
+                    i += 1
+                # also consume the empty line after the definition
+                existingLines.append(line)
+                line = lines[i].rstrip()
+                i += 1
+
+            newPropLines.append("    @property")
+            newPropLines.append("    def " + attributeName + "(self):")
+            newPropLines.append('        """')
+            newPropLines.append("        :type: " + attributeDocType)
+            newPropLines.append('        """')
             if isCompletable:
-                newLines.append(
+                newPropLines.append(
                     "        self._completeIfNotSet(self._" + attributeName + ")"
                 )
-            newLines.append("        return self._" + attributeName + ".value")
-            newLines.append("")
-            if isProperty:
-                newLines.append("    @property")
+            newPropLines.append("        return self._" + attributeName + ".value")
+            newPropLines.append("")
+
             added = True
+            existed = len(existingLines) > 0
+            updated = existed and existingLines != newPropLines
+
+            newLines.extend(newPropLines)
+        if isProperty and not existed:
+            newLines.append("    @property")
         isProperty = False
-    newLines.append(line)
+    if not isProperty:
+        newLines.append(line)
+
+if updated:
+    print(f'Updated {className}.{attributeName}')
+elif not existed:
+    print(f'Added {className}.{attributeName}')
 
 added = False
+existed = False
+updated = False
 
 inInit = line.endswith("def _initAttributes(self):")
 while not added:
@@ -129,14 +162,24 @@ while not added:
         if not line or line.endswith(" = github.GithubObject.NotSet"):
             if line:
                 attrName = line[14:-29]
-            if not line or attrName > attributeName:
-                newLines.append(
-                    "        self._" + attributeName + " = github.GithubObject.NotSet"
-                )
+            if not line or attrName >= attributeName:
+                newLine = "        self._" + attributeName + " = github.GithubObject.NotSet"
+                if attrName == attributeName:
+                    existed = True
+                    updated = line != newLine
+                newLines.append(newLine)
                 added = True
-    newLines.append(line)
+    if not existed:
+        newLines.append(line)
+
+if updated:
+    print(f'Updated {className}.{attributeName} in _initAttributes')
+elif not existed:
+    print(f'Added {className}.{attributeName} to _initAttributes')
 
 added = False
+existed = False
+updated = False
 
 inUse = False
 while not added:
@@ -151,14 +194,29 @@ while not added:
         if not line or line.endswith(" in attributes:  # pragma no branch"):
             if line:
                 attrName = line[12:-36]
-            if not line or attrName > attributeName:
-                newLines.append(
+            if not line or attrName >= attributeName:
+                # collect lines existing for this property and those new lines we generate
+                # to compare them and detect an update
+                newUseLines = []
+                existingLines = []
+
+                if attrName == attributeName:
+                    # consume all existing lines regarding this attribute
+                    while line.startswith(f'        if "{attributeName}"') or line.startswith('            '):
+                        existingLines.append(line)
+                        if i < len(lines):
+                            line = lines[i].rstrip()
+                        else:
+                            line = ""
+                        i += 1
+
+                newUseLines.append(
                     '        if "'
                     + attributeName
                     + '" in attributes:  # pragma no branch'
                 )
                 if attributeAssertType:
-                    newLines.append(
+                    newUseLines.append(
                         '            assert attributes["'
                         + attributeName
                         + '"] is None or isinstance(attributes["'
@@ -169,11 +227,24 @@ while not added:
                         + attributeName
                         + '"]'
                     )
-                newLines.append(
+                if existingLines.__len__() == 4:
+                    # this line has been reformatted as it is too long for one line
+                    attributeValue = attributeValue.replace("(", "(\n                ").replace(")", "\n            )")
+                newUseLines.append(
                     "            self._" + attributeName + " = " + attributeValue
                 )
+
                 added = True
+                existed = len(existingLines) > 0
+                updated = existed and existingLines != newUseLines
+
+                newLines.extend(newUseLines)
     newLines.append(line)
+
+if updated:
+    print(f'Updated {className}.{attributeName} in _useAttributes')
+elif not existed:
+    print(f'Added {className}.{attributeName} to _useAttributes')
 
 
 while i < len(lines):
